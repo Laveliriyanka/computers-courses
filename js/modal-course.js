95736198d9f2685fb3f4db4eaa594ad731c40bf0
unknown <maksim.ralevich@innowise-group.com>
2023-06-08 16:39:29 +0200

    add modal-course

---
+++ js/modal-course.js
@@ -0,0 +1,9 @@
+function openModal() {
+    document.getElementById('modal-course').classList.add('open');
+
+}
+
+function closeModal() {
+    document.getElementById('modal-course').classList.remove('open');
+
+}
